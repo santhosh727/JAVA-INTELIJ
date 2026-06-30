2b708d4e39344aad68224584b9ac9693c89d6ed3
santhosh <santhosh15032007@gmail.com>
2026-06-30 21:21:33 +0530

    program for dowhile loop

---
+++ src/day_6/Class_task/do_while_example.java
@@ -0,0 +1,11 @@
+package day_6.Class_task;
+
+public class do_while_example {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i++);
+        }
+        while(i<=7);
+    }
+}
