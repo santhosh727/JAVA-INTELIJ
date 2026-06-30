dedff36edc8e3399a2f8644acecf6aa137008a4c
santhosh <santhosh15032007@gmail.com>
2026-06-30 21:19:06 +0530

    program to print number pattern

---
+++ src/day_6/Class_task/program_to_print_number_pattern.java
@@ -0,0 +1,12 @@
+package day_6.Class_task;
+
+public class program_to_print_number_pattern {
+    public static void main( String [] args){
+        for (int i =1;i <=5; i++){
+            for (int j =1 ; j<=i;  j++){ //12345
+                System.out.print("\t"+ j);
+            }
+            System.out.println(); // takes to the next after the inner loop completely runs
+        }
+    }
+}
