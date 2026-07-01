d6a6076657d777ccaf1733483355bc12e6a11981
santhosh <santhosh15032007@gmail.com>
2026-07-01 09:33:54 +0530

    program to print elements in array

---
+++ src/day_7/Class_task/program_to_print_elements_in_array.java
@@ -0,0 +1,10 @@
+package day_7.Class_task;
+
+public class program_to_print_elements_in_array {
+    public static void main(String[] args) {
+        int [] nums ={4,5,6,7,8,9,10};
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
